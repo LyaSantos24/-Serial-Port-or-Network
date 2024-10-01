03c961b6b307014d5199ed7112b471ae803c3b0e
LyaSantos24 <147869869+LyaSantos24@users.noreply.github.com>
2024-09-30 21:06:16 -0400

    Update serialPort.close.cpp
    Function closeDoor(fstream serialPort):
    
        This function closes a file stream or serial port connection represented by the fstream object serialPort.
    
    Function closeDoor(SOCKET sock):
    
        This function closes a network socket connection represented by the SOCKET object sock using the closesocket() function, which is specific to Windows socket operations.
    
    In summary:
    
        The first function closes a file or serial port connection.
        The second function closes a network socket connection.

--- serialPort.close.cpp
+++ serialPort.close.cpp
@@ -17,3 +17,37 @@ void closeDoor(SOCKET sock) {
 	closesocket(sock);
 
 }
+
+
+
+
+
+//other version
+
+
+////bibliotecas utilizadas
+//#include <iostream> 
+//#include <fstream>
+//#include <string>
+
+
+//using namespace std;
+//fstream serial("COM7");           //porta COM7
+
+
+
+// void closeDoor() {             
+                      
+
+  // serial.close();
+
+ //  cout << "porta fechada" <<endl;
+
+
+//}
+
+//int main(){                      // função principal
+
+ //   closeDoor();               //função fechar a porta
+
+//}
